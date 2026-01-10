c2ec725d8683213d7189d28232b19747db5f3225
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-10 19:45:11 -0300

    Criando usuário e realizando autenticação

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
 from flask import Flask, request, jsonify
-from flask_login import LoginManager
-
+from models.user import User
 from database import db
-import models.user as User
+from flask_login import LoginManager, login_user, current_user
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
+#view login
+login_manager.login_view = 'login'
+#Session <- conexão ativa
 
-@app.route('/login', methods=['POST'])
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
+
+@app.route('/login', methods=["POST"])
 def login():
-    data = request.json
-    username = data['username']
-    password = data['password']
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
 
-    if username and password:
-        # Login
-        user = User.query.filter_by(username=username).first()
+  if username and password:
+    # Login
+    user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
-                return jsonify({"message": "Autenticação realizada com sucesso"}), 200
+    if user and user.password == password:
+      login_user(user)
+      print(current_user.is_authenticated)
+      return jsonify({"message": "Autenticação realizada com sucesso!"})
 
-    return jsonify({"message": "Credenciais inválidas"}), 400
+  return jsonify({"message": "Credenciais inválidas"}), 400
 
-@app.route("/hello-world", methods=['GET'])
+@app.route("/hello-world", methods=["GET"])
 def hello_world():
-    return 'Hello World'
+  return "Hello world"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@ from database import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+  # id (int), username (text), password (text)
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), nullable=False, unique=True)
+  password = db.Column(db.String(80), nullable=False)
