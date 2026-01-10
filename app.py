0209e543270a8e8335c66d5f6754993211b3435c
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-09 23:05:32 -0300

    Autenticação com Flask - login - parte 1

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_login import LoginManager
+
 from database import db
 import models.user as User
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data['username']
+    password = data['password']
+
+    if username and password:
+        pass
+
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 @app.route("/hello-world", methods=['GET'])
 def hello_world():
