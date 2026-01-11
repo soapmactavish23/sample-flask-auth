36e0f74263cd95960e26913fea870698b47c9bb3
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-11 18:12:24 -0300

    Gerenciamento de Perfil

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def create_user():
     password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password)
+        user = User(username=username, password=password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "Usuário cadastrado com sucesso!"})
@@ -73,6 +73,9 @@ def update_user(id_user):
     data = request.json
     user = User.query.get(id_user)
 
+    if id_user != current_user.id and current_user.role == 'user':
+        return jsonify({"message": "Operação não permitida"}), 403
+
     if user and data.get("password"):
         user.password = data.get("password")
         db.session.commit()
@@ -85,7 +88,7 @@ def update_user(id_user):
 def delete_user(id_user):
     user = User.query.get(id_user)
 
-    if id_user== current_user.id:
+    if id_user == current_user.id:
         return jsonify({"message": "Deleção não permitida"}), 403
 
     if user:
